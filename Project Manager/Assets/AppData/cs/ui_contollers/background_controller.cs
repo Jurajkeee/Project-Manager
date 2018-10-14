81efce5a05b5868d3dc506114e8f14b03f3110c5
Mobot <po4tajurajkeee@gmail.com>
2018-10-14 14:13:57 +0300

    Unity Development Started
    Started main menu

---
+++ Project Manager/Assets/AppData/cs/ui_contollers/background_controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class background_controller : MonoBehaviour {
+    private GameObject background;
+
+    private void Start()
+    {
+        background = this.gameObject;
+        if (background == null) Debug.LogError(this.name + "Background Not Found");       
+    }
+
+    private float FindAspectRatio(){
+        var imageComponent = background.GetComponent<Image>();
+        var width = imageComponent.sprite.bounds.size.x;
+        var height = imageComponent.sprite.bounds.size.y;
+        Debug.Log(this.name + "Aspect Ratio = " + width / height);
+        return width / height;
+    }
+
+    public void SetNewBackGroundImage(Sprite image){
+        background.GetComponent<Image>().sprite = image;
+        background.GetComponent<AspectRatioFitter>().aspectRatio = FindAspectRatio();
+    }    
+}
---
+++ Project Manager/Assets/AppData/cs/ui_contollers/ui_manager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ui_manager : MonoBehaviour {
+    [Space(5)]
+    [Header("Controll Panel")]
+    public background_controller backgroundController;
+
+    [Header("Assets")]
+    [Space(2)]
+
+    [Header("Background Sprites")]
+    public Sprite[] backgroundImage = new Sprite[6];
+
+
+    private void Start()
+    {
+
+    }
+  
+}
